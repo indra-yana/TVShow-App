9a4a9ec0c1b6524346922f4b7bb82930b223c314
Indra Muliana <indra.ndra26@gmail.com>
2020-11-07 17:06:20 +0700

    #4 Pagination In TV Show List, Endless Recycler View

--- app/src/main/java/com/training/tvshowapp/activities/MainActivity.java
+++ app/src/main/java/com/training/tvshowapp/activities/MainActivity.java
@@ -1,10 +1,13 @@
 package com.training.tvshowapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.training.tvshowapp.R;
 import com.training.tvshowapp.adapters.TVShowsAdapter;
@@ -17,10 +20,14 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     private MostPopularTVShowsViewModel viewModel;
     private ActivityMainBinding activityMainBinding;
     private List<TVShow> tvShows = new ArrayList<>();
     private TVShowsAdapter tvShowsAdapter;
+    private int currentPage = 1;
+    private int totalAvailablePage = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,21 +42,58 @@ public class MainActivity extends AppCompatActivity {
         tvShowsAdapter = new TVShowsAdapter(tvShows);
         activityMainBinding.rvTvShowList.setHasFixedSize(true);
         activityMainBinding.rvTvShowList.setAdapter(tvShowsAdapter);
+        activityMainBinding.rvTvShowList.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (!activityMainBinding.rvTvShowList.canScrollVertically(1)) {
+                    if (currentPage <= totalAvailablePage) {
+                        currentPage++;
+                        getMostPopularTVShows();
+
+                        Log.d(TAG, "End of recyclerview!");
+                    }
+                }
+            }
+        });
 
         getMostPopularTVShows();
     }
 
     private void getMostPopularTVShows() {
-        activityMainBinding.setIsLoading(true);
+        toggleLoading();
 
-        viewModel.getMostPopularTVShows(0).observe(this, tvShowResponse -> {
-            activityMainBinding.setIsLoading(false);
+        viewModel.getMostPopularTVShows(currentPage).observe(this, tvShowResponse -> {
+            toggleLoading();
             if (tvShowResponse != null) {
+                totalAvailablePage = tvShowResponse.getTotalPages();
                 if (tvShowResponse.getTvShows() != null) {
+                    int oldCount = tvShows.size();
+
                     tvShows.addAll(tvShowResponse.getTvShows());
-                    tvShowsAdapter.notifyDataSetChanged();
+                    tvShowsAdapter.notifyItemRangeInserted(oldCount, tvShows.size());
                 }
+
+                Log.d(TAG, "Delivered result!");
+            } else {
+                Log.d(TAG, "Something went wrong!");
             }
         });
+    }
+
+    private void toggleLoading() {
+        if (currentPage == 1) {
+            if (activityMainBinding.getIsLoading() != null && activityMainBinding.getIsLoading()) {
+                activityMainBinding.setIsLoading(false);
+            } else {
+                activityMainBinding.setIsLoading(true);
+            }
+        } else {
+            if (activityMainBinding.getIsLoadingMore() != null && activityMainBinding.getIsLoadingMore()) {
+                activityMainBinding.setIsLoadingMore(false);
+            } else {
+                activityMainBinding.setIsLoadingMore(true);
+            }
+        }
     }
 }
